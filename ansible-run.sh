3c78bc214069381e57510457af5474e18f3398f6
srikvp <93551165+srikvp@users.noreply.github.com>
2024-11-30 19:10:32 +0530

    correcting vault config

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,4 +1,4 @@
-
+# $1 value can be the ip address or the dns name of the server we wish to connect to
 
 
 ansible-playbook -i $1, tool-setup.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e tool_name=github-runner
