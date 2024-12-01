083b32b64714e2a0e1128954677ce4e07f4e9f3c
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-01 10:29:30 +0530

    defining and using organization secrets from actions for better security

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,4 +1,5 @@
 # $1 value can be the ip address or the dns name of the server we wish to connect to
 
 
-ansible-playbook -i $1, tool-setup.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e tool_name=github-runner
+# shellcheck disable=SC2296
+ansible-playbook -i $1, tool-setup.yml -e ansible_user=${{ secrets.SSH_USER }} -e ansible_password=${{ secrets.SSH_PWD }} -e tool_name=github-runner
