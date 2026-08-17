928bfa087c0df2cd88c07c9d7a2b56f2fff4a3d0
sri <sriramrmk@gmail.com>
2026-08-16 20:11:24 -0400

    updated comments

--- infra-create/iam.tf
+++ infra-create/iam.tf
@@ -1,8 +1,10 @@
-#creating an iam role(github-runner-role) to be attached to the github runner server
+# creating an iam role(github-runner-role) to be attached to the github runner server
 
 resource "aws_iam_role" "role" {
   name = "${var.name}-role"
-  #we are modeling this role after a pre-existing role - workstation role. go to trust relationship version and statement info in that role to fill in the info below. name = github-runner-role
+  # modeling this role after a pre-existing role - workstation role. 
+  # go to trust relationship version and statement info in that role to fill in the info below. 
+  # name = github-runner-role
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -20,18 +22,21 @@ resource "aws_iam_role" "role" {
   }
 }
 
-#ARN can be attached to SAS and PAS services. For IAS such as ec2, we need to attach Instance profile ARN as well
+# Instance profile is a wrapper for ec2 roles
+# they do not contain the permission themselves
+# they point to ec2 roles
+# they are attached to ec2 instances
 resource "aws_iam_instance_profile" "instance-profile" {
   name = "${var.name}-role"
   role = aws_iam_role.role.name
 }
 
-#attaching policy
-#count is being used since policy_name could be made a list variable.
+# attaching policy
+# count is being used since policy_name could be made a list variable.
 resource "aws_iam_role_policy_attachment" "policy-attach" {
   count      = length(var.policy_name)
   role       = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/${var.policy_name[count.index]}"
-  #policy_arn info is obtained from aws policy AdministratorAccess.
-  #policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  # policy_arn info is obtained from aws policy AdministratorAccess.
+  # policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
